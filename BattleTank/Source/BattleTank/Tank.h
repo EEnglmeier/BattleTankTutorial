c9974d81e298cd5e2aad324960fbad30b9aa6e00
EEnglmeier <elias_englmeier@yahoo.de>
2016-11-22 23:54:48 +0100

    Movement

---
+++ BattleTank/Source/BattleTank/Tank.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BattleTank.h"
+#include "GameFramework/Pawn.h"
+#include "Tank.generated.h"
+
+class UTankBarrel;
+class UTankAimingComponent;
+class UTankMovementComponent;
+class AProjectile;
+class UTankTrack;
+
+UCLASS()
+class BATTLETANK_API ATank : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	void AimAt(FVector HitLocation);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UTankBarrel* BarrelToSet);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetTurretReference(UTankTurret* TurretToSet);
+
+	UFUNCTION(BlueprintCallable,Category=Firing)
+	void Fire();
+
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
+	UPROPERTY(BlueprintReadonly)
+	UTankMovementComponent* TankMovementComponent = nullptr;
+
+private:
+	// Sets default values for this pawn's properties
+	ATank();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
+	float LaunchSpeed = 4000;
+
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
+	TSubclassOf<AProjectile> ProjectileBlueprint;
+	
+	UTankBarrel* Barrel = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
+	float ReloadTimeInSeconds = 3.0f;
+	
+	double LastFireTime = 0;
+};
---
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+class ATank;
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+private:
+	ATank* 	GetControlledTank() const;
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	void AimTowardsCrosshair();
+
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+	UPROPERTY(EditDefaultsOnly)
+	float CrossHairXLocation =.5f;
+	
+	UPROPERTY(EditDefaultsOnly)
+	float CrossHairYLocation=0.3333f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float LineTraceRange = 1000000;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
+};
